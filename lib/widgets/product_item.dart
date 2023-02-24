515d96e9573f12aac50ee6a7447e75f1f295cbbc
Daan Enderrs <daan@posonas.com>
2023-02-24 14:02:47 +0100

    color change

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -34,7 +34,7 @@ class ProductItem extends StatelessWidget {
           leading: IconButton(
             icon: Icon(
                 product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Colors.red[600],
+            color: Color.fromARGB(255, 201, 53, 50),
             onPressed: () {
               product.toggleFavorite();
             },
